796ebfbb34ec2f4fe8d824f8ae62ff87f7f06d8b
赵安家 <anjia0532@gmail.com>
2022-05-11 18:52:21 +0800

    doc: 添加 32 位 二进制文件

--- scripts/build.bat
+++ scripts/build.bat
@@ -17,9 +17,19 @@ SET GOARCH=amd64
 go build -o ../.build/discovery-syncer-%GOOS%-%GOARCH%.exe
 
 SET CGO_ENABLED=0
+SET GOOS=windows
+SET GOARCH=386
+go build -o ../.build/discovery-syncer-%GOOS%-%GOARCH%.exe
+
+SET CGO_ENABLED=0
 SET GOOS=linux
 SET GOARCH=amd64
 go build -o ../.build/discovery-syncer-%GOOS%-%GOARCH%
 
+
+SET CGO_ENABLED=0
+SET GOOS=linux
+SET GOARCH=386
+go build -o ../.build/discovery-syncer-%GOOS%-%GOARCH%
 
 cd /d %mypath%
